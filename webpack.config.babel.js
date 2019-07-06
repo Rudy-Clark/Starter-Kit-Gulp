b2e8621a32a349ce587c17a12473b21cc5a7211c
Rudy Clark <rudy.uzb@gmail.com>
2019-07-06 16:27:03 +0500

    add new webpack@4 and new configuration

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,74 +1,31 @@
-
+import { config } from 'dotenv'
 import path from 'path'
-import webpack from 'webpack'
+import UglifyJsPlugin from 'uglifyjs-webpack-plugin'
 
-import { browsers } from './gulpfile.babel'
-
-const stats = {
-    hash: false,
-    version: false,
-    timings: false,
-    assets: true,
-    chunks: false,
-    modules: false,
-    reasons: false,
-    children: false,
-    source: false,
-    errors: true,
-    errorDetails: true,
-    warnings: true,
-    publicPath: false
-}
+config()
+const entry = process.env.JS ? `./src/js/${process.env.JS}` : './src/js/main'
 
 export default {
-    devtool: 'source-map',
-    entry: './src/js/main.js',
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'build/js/'),
-        publicPath: './build/js/',
-        sourceMapFilename: '[name].map.js'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-            query: {
-                presets: [['env', {
-                    modules: false,
-                    targets: { browsers }
-                }]]
-            }
-        }]
-    },
-    resolve: {
-        alias: {
-            '~': path.resolve(__dirname, 'src/js/')
+  mode: process.env.NODE_ENV,
+  entry,
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader'
         }
-    },
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'common',
-            minChunks: 2
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            minimize: true,
-            sourceMap: true,
-            mangle: false,
-            compressor: {
-                warnings: false,
-                screw_ie8: true
-            },
-            output: {
-                comments: false
-            }
-        }),
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-        })
-    ],
-    stats
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin()
+    ]
+  }
 }
